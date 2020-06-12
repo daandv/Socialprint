7fa2651218ff76e4a87a93e5047de967735ddf12
Your Namdaandv <daandevry2@gmailcom>
2020-06-13 01:28:20 +0200

    404 pagina aangemaakt

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="centered">
+    <h1 class="bigTitle">Pagina bestaat niet</h1>
+    <h1 class="bigTitle">&#128581;&#128581;&#128581;</h1>
+  </div>
+</div>
+
+
+@include('layouts.footer')
+
+@endsection
