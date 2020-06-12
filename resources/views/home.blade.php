55d60da4075d28ebae57fc74bf75997e73dff341
Your Namdaandv <daandevry2@gmailcom>
2020-06-13 01:17:51 +0200

    Tip aan vraagteken toegevoegd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,7 +26,7 @@
         <input type="text" id="adress" placeholder="Zoek in een gemeente">
       </form>
       <img onclick="location.href='{{route('welcome')}}';" class="logo_icon" src="{{ asset('images/logo_icon.png')}}">
-      <span onclick="location.href='{{route('faq')}}';" class="faq_icon">&#10067;</span>
+      <span onclick="location.href='{{route('faq')}}';" class="faq_icon" data-tippy-content="Ik begrijp iets niet." data-tippy-arrow ="false" data-tippy-placement="left" data-tippy-animation="scale-subtle">&#10067;</span>
     </div>
 
 
