16da2ce13f51ad12b0df14e4352b70e416c01d50
Your Namdaandv <daandevry2@gmailcom>
2020-06-12 19:41:30 +0200

    Fonts mobile fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,9 +34,9 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Arapey:ital@0;1&display=swap" rel="stylesheet">
 
 </head>
 <body>
